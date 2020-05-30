b220cd2237e55f0c0af487831a0bbbf0c625edf1
shekhar_a_agrawal <shekhar.a.agrawal@oracle.com>
2020-05-30 01:01:40 -0500

    first

--- notebooks/labs/common.py
+++ notebooks/labs/common.py
@@ -1,13 +1,22 @@
 from pyspark import *
 from pyspark.streaming import *
 from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+
 
 class Common:
     def __init__(self, app_name='myapp', master='local'):        
         conf = SparkConf().setAppName(app_name).setMaster(master)
         self.sc = SparkContext(conf=conf)
         
-    def get_sc(self):
+    def get_spark_core(self):
         print (self.sc)
         print (self.sc.version)
         return self.sc
+    
+    def get_spark_sql(self):
+        spark = SparkSession.builder.appName("Spark SQL basic example").enableHiveSupport().getOrCreate()
+        sc = spark.sparkContext
+        print (sc)
+        print (sc.version)
+        return sc,spark
